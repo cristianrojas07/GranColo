8307d8eee4c0e00daa38cc52ae9b444358ec5e50
cristianrojas07 <cristianjesusrojas.chechu@gmail.com>
2019-09-12 23:05:48 -0300

    Agregado capas, login y frmPrincipal

--- src/GranColo/Program.cs
+++ src/GranColo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GranColo.GUILayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace GranColo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPrincipal());
         }
     }
 }
