264c0b758c3827dc37b98fc83d00487c5b4bdcc8
franciscozapata12346 <franciscozapata12346@gmail.com>
2019-09-15 06:41:01 -0300

    Creacion de un DT de prueba, login funcionando y agregado de clases a las capas

--- src/GranColo/BusinessLayer/Entities/DirectorTecnico.cs
+++ src/GranColo/BusinessLayer/Entities/DirectorTecnico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GranColo.BusinessLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,23 @@ namespace GranColo.BusinessLayer
 {
     class DirectorTecnico
     {
+        private TipoDocumento tipoDoc;
+
         public int idDirectorTecnico { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
         public int nroDoc { get; set; }
-        public int tipoDoc { get; set; }
+
+        //public TipoDocumento tipoDoc { get; set; }
         public string nombreUsuario { get; set; } 
         public string password { get; set; }
         public string email { get; set; }
         public int idClubHincha { get; set; }
+
+        public DirectorTecnico()
+        {
+            tipoDoc = new TipoDocumento();
+        }
 
     }
 }
